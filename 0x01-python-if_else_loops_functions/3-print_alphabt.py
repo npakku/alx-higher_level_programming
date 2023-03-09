b8f83653fd89788020d9c60d3d74ffa60836a555
npakku <pascal.naa.zury@gmail.com>
2023-03-09 05:01:30 -0800

    print and exclude some alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -4,4 +4,4 @@ for i in range(ord('a'), ord('z') + 1):
     if (chr(i) == 'e' or chr(i) == 'q'):
         continue
 
-    print("{}".format(chr(i)), end = '')
+    print("{}".format(chr(i)), end='')
