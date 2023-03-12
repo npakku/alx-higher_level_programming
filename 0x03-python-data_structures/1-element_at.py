08b4aaa9e560a10533b8570c41304151c3e86a03
npakku <pascal.naa.zury@gmail.com>
2023-03-12 01:10:12 -0800

    Retrieve element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,7 @@
 def element_at(my_list, idx):
     '''retrieve element from given index'''
 
-    for i in range(len(my_list)):
-        if idx < 0 or idx > len(my_list) - 1:
-            return None
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
 
-    return my_list[idx]
+    return (my_list[idx])
