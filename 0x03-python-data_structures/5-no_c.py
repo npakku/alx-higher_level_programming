37cc155887835237f3f69741e6d55deae7762a70
npakku <pascal.naa.zury@gmail.com>
2023-03-13 07:01:51 -0700

    Revome some characters from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    copy = [character for character in my_string if character != 'c' and character != 'C']
+    copy = [character for character in \
+            my_string if character != 'c' and character != 'C']
     return ("".join(copy))
