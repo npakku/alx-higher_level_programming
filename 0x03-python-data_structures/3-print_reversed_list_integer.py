caafbe5d406f6cccec851fc56978eaadf64a40b6
npakku <pascal.naa.zury@gmail.com>
2023-03-13 05:28:59 -0700

    Reverse a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-#Author: Pascal<pascal.naa.zury@gmail.com>
 
 def print_reversed_list_integer(my_list=[]):
     '''print list in reverse order'''
-    
     rev = []
     for i in range(-1, -6, -1):
         rev.append(my_list[i])
 
     for i in range(len(rev)):
         print("{}".format(rev[i]))
-  
