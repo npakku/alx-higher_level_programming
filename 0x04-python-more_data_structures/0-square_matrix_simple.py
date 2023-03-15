f6cd28975880c08ff93e04df2259bd89cd964a66
npakku <pascal.naa.zury@gmail.com>
2023-03-15 05:40:20 -0700

    square each element of 2D array

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda x : [element**2 for element in x], matrix))
+    return list(map(lambda x: [element**2 for element in x], matrix))
