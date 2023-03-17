ad2b8f696bd644e5126c2463d7277f0821eb60d1
npakku <pascal.naa.zury@gmail.com>
2023-03-17 04:23:55 -0700

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,9 +2,9 @@
 
 def update_dictionary(a_dictionary, key, value):
     """Replace or add key/value pairs in a dictionary."""
-    key_list = []
+    #key_list = []
     for keys in a_dictionary.copy():
-        key_list.append(key)
+     #   key_list.append(key)
         if key not in keys:
             a_dictionary[key] = value
     return a_dictionary
