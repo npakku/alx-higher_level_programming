ba447995f7a1c5eccb845ced8b578be48c5c14b6
npakku <pascal.naa.zury@gmail.com>
2023-05-17 06:42:49 -0700

    list recores

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
---select scores and names order by scores descending
+-- select all recoreds of second_table
+-- records are ordered in descending score
 SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
